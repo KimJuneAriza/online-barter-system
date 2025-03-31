6ab979f3be51d70d3079793b00834f35ebaf07e7
kim <kimjuneariza@gmail.com>
2025-03-31 11:16:30 +0800

    making the spinner a component

--- Online-Barter-System/src/app/main/modules/main.module.ts
+++ Online-Barter-System/src/app/main/modules/main.module.ts
@@ -5,11 +5,12 @@ import { RoutesModule } from './routes.module';
 import { SignupComponent } from '../pages/signup/signup.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { SigninComponent } from '../pages/signin/signin.component';
+import { SpinnerComponent } from '../shared/spinner/spinner.component';
 
 
 
 @NgModule({
-  declarations: [LandingComponent, SignupComponent, SigninComponent],
+  declarations: [LandingComponent, SignupComponent, SigninComponent,SpinnerComponent],
   imports: [
     CommonModule,
     RoutesModule,
